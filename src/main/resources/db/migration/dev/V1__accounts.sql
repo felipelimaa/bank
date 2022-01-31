e434b9a73c676869eb06eb5365e5c5c791986f07
felipe <fearlimasi@gmail.com>
2022-01-31 16:34:01 -0300

    feat: refatorado application.properties para yaml e adicionado flyway para migração dos dados.

---
+++ src/main/resources/db/migration/dev/V1__accounts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.accounts
+(
+    account_id bigint NOT NULL,
+    document_number character varying(255),
+    credit_avaiable numeric(19,2) NOT NULL DEFAULT 1000,
+    CONSTRAINT accounts_pkey PRIMARY KEY (account_id)
+);
+
+CREATE SEQUENCE public.hibernate_sequence
+    INCREMENT 1
+    START 1;
