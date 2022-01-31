2bab634c4234930f1deb2a9e60ed541a58906476
felipe <fearlimasi@gmail.com>
2022-01-31 19:20:07 -0300

    feat: alterado o @RequestMapping de Accounts e criado método findById no Service de Accounts.

--- src/main/groovy/br/com/bank/domain/service/AccountsService.groovy
+++ src/main/groovy/br/com/bank/domain/service/AccountsService.groovy
@@ -1,6 +1,8 @@
 package br.com.bank.domain.service
 
 import br.com.bank.domain.exception.AccountsDocumentNumberExistsException
+import br.com.bank.domain.exception.AccountsNotFoundException
+import br.com.bank.domain.exception.AccountsWithoutCreditAvailableException
 import br.com.bank.domain.exception.AccountsWithoutDocumentNumberException
 import br.com.bank.domain.model.Accounts
 import br.com.bank.domain.repository.AccountsRepository
@@ -19,6 +21,10 @@ class AccountsService {
         return accountsRepository.findAll()
     }
 
+    Accounts findById(Long id) {
+        return accountsRepository.findById(id).orElseThrow{ new AccountsNotFoundException() }
+    }
+
     @Transactional
     Accounts create(Accounts account) {
         // Feature for check if exists document number within payload
@@ -34,6 +40,17 @@ class AccountsService {
         }
 
         return accountsRepository.save(account)
+    }
+
+    void transactionValue(Accounts accounts, BigDecimal value) {
+        accounts.creditAvaiable += value
+
+        // verify if accounts without credit available
+        if(accounts.creditAvaiable < 0) {
+            throw new AccountsWithoutCreditAvailableException()
+        }
+
+        accountsRepository.save(accounts)
     }
 
 }
